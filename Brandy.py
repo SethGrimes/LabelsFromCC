c1ce71cd0c50f818a476231feefa5bf0eb026466
Seth Grimes <grimes@altaplana.com>
2021-12-03 12:17:23 -0500

    Update Brandy.py

--- Brandy.py
+++ Brandy.py
@@ -4,7 +4,7 @@ with open('ContributionsList-BrandyBrooks-2021Dec2.csv', newline='') as csvfile:
    i = 0
    for row in line:
       i += 1
-      if i >1:
+      if i >1 and i<11:
          rawname = row[7]
-         rawaddr = r[9]
-         print(', '.join((rawnamme,rawaddr)))
+         rawaddr = row[9]
+         print(', '.join((rawname,rawaddr)))
